6c9c70eaf96ceb4fc62884461b79561b97c939ba
Nesffer <nesffer.jimin@gmail.com>
2017-08-01 21:37:23 +0900

    Change property string to variable

--- src/config.example.js
+++ src/config.example.js
@@ -1,7 +1,7 @@
 module.exports = {
-  'NAVER_API_KEY_ID': process.env.NAVER_API_KEY_ID || '',
-  'NAVER_API_KEY_SECRET': process.env.NAVER_API_KEY_SECRET || '',
-  'KAKAO_API_KEY': process.env.KAKAO_API_KEY || '',
-  'OPENWEATHERMAP_API_KEY': process.env.OPENWEATHERMAP_API_KEY || '',
-  'GOOGLE_API_KEY': process.env.GOOGLE_API_KEY || ''
+  NAVER_API_KEY_ID: process.env.NAVER_API_KEY_ID || '',
+  NAVER_API_KEY_SECRET: process.env.NAVER_API_KEY_SECRET || '',
+  KAKAO_API_KEY: process.env.KAKAO_API_KEY || '',
+  OPENWEATHERMAP_API_KEY: process.env.OPENWEATHERMAP_API_KEY || '',
+  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || ''
 }
